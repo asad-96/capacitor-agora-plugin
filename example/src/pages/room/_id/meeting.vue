a96a5292c59f5b1f86fb8cab7b8e5034d22ab020
JohnMichaels <samcomptepriv@gmail.com>
2023-01-17 07:44:01 +0700

    feat: folder structure

--- example/src/pages/room/_id/meeting.vue
+++ example/src/pages/room/_id/meeting.vue
@@ -5,12 +5,23 @@
         <h1>you are in meeting room</h1>
       </v-col>
     </v-row>
+    <meeting-room-container @click:chat="handleChat"></meeting-room-container>
+    <v-navigation-drawer v-show="showChat"></v-navigation-drawer>
   </v-container>
 </template>
 <script lang="ts">
+import { ref } from '@nuxtjs/composition-api'
 import { defineComponent } from '@vue/composition-api'
+// import { CapacitorPluginAgora } from '@wellcare/capacitor-plugin-agora'
 
 export default defineComponent({
-  setup() {}
+  setup() {
+    const showChat = ref(false)
+    const handleChat = () => {
+      showChat.value = true
+      // CapacitorPluginAgora.enablePip()
+    }
+    return { handleChat }
+  }
 })
 </script>
